6ca046bd8a8805236ebe02902e2bf2e197b287fd
Manish S B <manish.manu62@gmail.com>
2019-06-23 20:05:46 +0530

    add jwt authentication to get APIs

---
+++ middleware/index.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const checkToken = (req, res, next) => {
+  const token = req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, 'secret_token', (err, decoded) => {
+      if (err) {
+        res.send({
+          statusCode: 401,
+          message: err.message,
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.send({
+      statusCode: 401,
+      message: 'Unauthorized',
+    });
+  }
+};
+
+module.exports = {
+  checkToken,
+};
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const bankHandlers = require('./handlers/bank.handler');
+const loginHandler = require('./handlers/login.handler');
+const accessMiddleware = require('./middleware');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/ifsc/:ifsc', bankHandlers.findByIFSC);
-app.get('/bank/:bank/:city', bankHandlers.findByBankAndCity);
+app.get('/ifsc/:ifsc', accessMiddleware.checkToken, bankHandlers.findByIFSC);
+app.get('/bank/:bank/:city', accessMiddleware.checkToken, bankHandlers.findByBankAndCity);
+app.post('/login', loginHandler.login);
 app.get('*', bankHandlers.defaultRoute);
 app.listen(process.env.PORT || 3030);
