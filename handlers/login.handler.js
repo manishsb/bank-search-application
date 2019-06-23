001b3149c6e35a5e8e98ddafa7d0ad8ae11a6438
Manish S B <manish.manu62@gmail.com>
2019-06-23 20:35:58 +0530

    replace secret value by environment variables

--- handlers/login.handler.js
+++ handlers/login.handler.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const login = (req, res) => {
   if (req.body.user) {
-    const token = jwt.sign({ user: req.body.user }, 'secret_token', { expiresIn: '5 days' });
+    const token = jwt.sign({ user: req.body.user }, process.env.SECRET_KEY, { expiresIn: '5 days' });
     res.send({ token });
   }
 };
