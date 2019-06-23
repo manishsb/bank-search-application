409996745f80eb8337343488d08512a78253fd85
Manish S B <manish.manu62@gmail.com>
2019-06-23 18:56:47 +0530

    validate query params value

--- handlers/bank.handler.js
+++ handlers/bank.handler.js
@@ -1,23 +1,42 @@
 const Model = require('../models');
 
+const getLimitAndOffset = ({ limit, offset }) => {
+  const limitVal = parseInt(limit, 10);
+  const offsetVal = parseInt(offset, 10);
+  const queryObject = {};
+  if (limitVal) {
+    queryObject.limit = limitVal;
+  }
+  if (offsetVal) {
+    queryObject.offset = offsetVal;
+  }
+  return queryObject;
+};
+
+const findByIFSC = (req, res) => {
+  const { ifsc } = req.params;
+  Model.bank_branch.findByIfsc(
+    ifsc, getLimitAndOffset(req.query),
+  ).then((entry) => {
+    res.send(entry);
+  });
+};
+
+const findByBankAndCity = (req, res) => {
+  const { bank, city } = req.params;
+  Model.bank_branch.findAllByBankAndCity(
+    bank, city, getLimitAndOffset(req.query),
+  ).then((entries) => {
+    res.send(entries);
+  });
+};
+
+const defaultRoute = (req, res) => {
+  res.status(404).send('Page Not Found');
+};
+
 module.exports = {
-  findByIFSC: (req, res) => {
-    const { ifsc } = req.params;
-    const { limit, offset } = req.query;
-    Model.bank_branch.findByIfsc(ifsc, { limit, offset }).then((entry) => {
-      res.send(entry);
-    });
-  },
-
-  findByBankAndCity: (req, res) => {
-    const { bank, city } = req.params;
-    const { limit, offset } = req.query;
-    Model.bank_branch.findAllByBankAndCity(bank, city, { limit, offset }).then((entries) => {
-      res.send(entries);
-    });
-  },
-
-  defaultRoute: (req, res) => {
-    res.status(404).send('Page Not Found');
-  },
+  findByIFSC,
+  findByBankAndCity,
+  defaultRoute,
 };
